67c28692470a801b9b3d6e7ff79496c4de0e88a1
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-07 16:16:34 +0300

    Implement process checkpointing with CRIU
    Added a function to checkpoint a process using CRIU.

--- universal-teleportation/state-capture/criu_wrapper.py
+++ universal-teleportation/state-capture/criu_wrapper.py
@@ -1,1 +1,17 @@
+#Interface to CRIU commands.
+# Uses subprocess.
+# Example:
 
+import subprocess
+
+def checkpoint_process(pid, directory):
+
+    cmd = [
+        "criu",
+        "dump",
+        "-t", str(pid),
+        "-D", directory,
+        "--shell-job"
+    ]
+
+    subprocess.run(cmd)
