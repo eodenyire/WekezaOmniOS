75e39d95ec73776d18606baadb905e29eaa009fc
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-07 17:00:31 +0300

    Initialize FastAPI server with basic route

--- universal-teleportation/api/server.py
+++ universal-teleportation/api/server.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from routes import router
 
+app = FastAPI(
+    title="WekezaOmniOS Universal Teleportation API",
+    description="API for controlling Universal Application Teleportation",
+    version="0.1"
+)
+
+app.include_router(router)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "WekezaOmniOS Universal Teleportation API",
+        "status": "running"
+    }
