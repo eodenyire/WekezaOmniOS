d7232bf69306d159012eed71118cb9c9b59f6e13
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-07 16:27:53 +0300

    Add main entry point and example usage to teleport.py
    Added a main entry point and example usage for the teleport script.

--- universal-teleportation/cli/teleport.py
+++ universal-teleportation/cli/teleport.py
@@ -1,1 +1,14 @@
+# teleport.py
+# Main entry point.
+# Example usage:
+#python teleport.py capture 1921
 
+import sys
+
+def main():
+
+    command = sys.argv[1]
+
+    if command == "capture":
+        pid = sys.argv[2]
+        print(f"Capturing {pid}")
