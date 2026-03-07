b53fe3415070907268f19d13b3dbad0285a75c96
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-07 16:26:15 +0300

    Implement CRIU restore wrapper in criu_restore.py
    Added a wrapper function for CRIU restore functionality.

--- universal-teleportation/state-reconstruction/criu_restore.py
+++ universal-teleportation/state-reconstruction/criu_restore.py
@@ -1,1 +1,16 @@
+# criu_restore.py
+# Wrapper around CRIU restore.
+# Example:
 
+import subprocess
+
+def restore(directory):
+
+    cmd = [
+        "criu",
+        "restore",
+        "-D", directory,
+        "--shell-job"
+    ]
+
+    subprocess.run(cmd)
