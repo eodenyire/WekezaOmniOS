11d329dc05b81ce2fb968119eaa95db8c1cc252a
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:47:51 +0300

    Implement DirtyPageTracker class for page tracking

--- universal-teleportation/live-migration/dirty_page_tracker.py
+++ universal-teleportation/live-migration/dirty_page_tracker.py
@@ -1,1 +1,13 @@
+class DirtyPageTracker:
 
+    def __init__(self):
+
+        self.dirty_pages = []
+
+    def mark_dirty(self, page):
+
+        self.dirty_pages.append(page)
+
+    def get_dirty_pages(self, process_id):
+
+        return self.dirty_pages
