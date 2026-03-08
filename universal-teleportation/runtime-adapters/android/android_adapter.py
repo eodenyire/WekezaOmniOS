d77219f6fc90855464dddbc68d146c483a3ab88c
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 18:46:50 +0300

    Add AndroidAdapter class with environment methods

--- universal-teleportation/runtime-adapters/android/android_adapter.py
+++ universal-teleportation/runtime-adapters/android/android_adapter.py
@@ -1,1 +1,9 @@
+class AndroidAdapter:
 
+    def prepare_environment(self, snapshot):
+
+        print("Preparing Android runtime")
+
+    def restore_process(self, snapshot):
+
+        print("Restoring Android app")
