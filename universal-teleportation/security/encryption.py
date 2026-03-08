b5107acc49a4e2ed25a82b3a59f53cb573ffad74
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:06:40 +0300

    Implement EncryptionManager for data encryption

--- universal-teleportation/security/encryption.py
+++ universal-teleportation/security/encryption.py
@@ -1,1 +1,16 @@
+from cryptography.fernet import Fernet
 
+class EncryptionManager:
+
+    def __init__(self):
+
+        self.key = Fernet.generate_key()
+        self.cipher = Fernet(self.key)
+
+    def encrypt(self, data):
+
+        return self.cipher.encrypt(data)
+
+    def decrypt(self, data):
+
+        return self.cipher.decrypt(data)
