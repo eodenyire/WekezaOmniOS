b881d1a4eedfe5b4ced7ae1ce2f837e009ba344f
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:48:29 +0300

    Add PageBitmap class for page state management
    Implement PageBitmap class to manage page states.

--- universal-teleportation/live-migration/page_bitmap.py
+++ universal-teleportation/live-migration/page_bitmap.py
@@ -1,1 +1,13 @@
+class PageBitmap:
 
+    def __init__(self, size):
+
+        self.bitmap = [0] * size
+
+    def mark(self, page):
+
+        self.bitmap[page] = 1
+
+    def get_dirty(self):
+
+        return [i for i, v in enumerate(self.bitmap) if v == 1]
