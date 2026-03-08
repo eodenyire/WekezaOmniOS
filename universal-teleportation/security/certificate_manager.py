7f0c416c4fbf014c9636b6ea926b531f376917bb
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:28:27 +0300

    Add CertificateManager class for certificate handling

--- universal-teleportation/security/certificate_manager.py
+++ universal-teleportation/security/certificate_manager.py
@@ -1,1 +1,11 @@
+import secrets
 
+class CertificateManager:
+
+    def generate_certificate(self):
+
+        return secrets.token_hex(32)
+
+    def validate_certificate(self, cert):
+
+        return len(cert) > 20
