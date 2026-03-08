4070bfacaa4018b2362816eb7b13788fd243a5b8
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:17:33 +0300

    Update container_restore.py

--- universal-teleportation/runtime-adapters/container_restore.py
+++ universal-teleportation/runtime-adapters/container_restore.py
@@ -1,1 +1,14 @@
+import subprocess
 
+def restore_container(container_id, checkpoint_name):
+    """
+    Restore container from checkpoint.
+    """
+
+    subprocess.run([
+        "docker",
+        "start",
+        "--checkpoint",
+        checkpoint_name,
+        container_id
+    ])
