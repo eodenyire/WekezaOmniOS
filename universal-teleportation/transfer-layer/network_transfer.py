caf48c62bc0e282611ee583544c5c36f7fc7b0c0
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 14:45:51 +0300

    Add send_snapshot function for network transfer

--- universal-teleportation/transfer-layer/network_transfer.py
+++ universal-teleportation/transfer-layer/network_transfer.py
@@ -6,3 +6,12 @@ def send_snapshot(snapshot_path, target_host, target_path):
     Transfer snapshot to remote node
     """
     shutil.copy(snapshot_path, target_path)
+
+
+import shutil
+
+def send_snapshot(snapshot_path, target_path):
+    """
+    Simulated network transfer (Phase 2 prototype).
+    """
+    shutil.copy(snapshot_path, target_path)
