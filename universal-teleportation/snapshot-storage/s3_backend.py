4b6b8c1de2092a612b2b145beb4a47931fbbd5b4
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:28:31 +0300

    Update s3_backend.py

--- universal-teleportation/snapshot-storage/s3_backend.py
+++ universal-teleportation/snapshot-storage/s3_backend.py
@@ -1,1 +1,21 @@
+import boto3
 
+class S3Backend:
+
+    def __init__(self, bucket):
+        self.s3 = boto3.client("s3")
+        self.bucket = bucket
+
+    def save(self, snapshot_path, snapshot_id):
+
+        for file in snapshot_path:
+
+            self.s3.upload_file(
+                file,
+                self.bucket,
+                f"{snapshot_id}/{file}"
+            )
+
+    def load(self, snapshot_id):
+
+        return f"s3://{self.bucket}/{snapshot_id}"
