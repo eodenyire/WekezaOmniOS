e0064765ff22c1a52d938410ceba5e4d26941ccd
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:29:16 +0300

    Implement AccessControl class for permission management

--- universal-teleportation/security/access_control.py
+++ universal-teleportation/security/access_control.py
@@ -1,1 +1,13 @@
+class AccessControl:
 
+    def __init__(self):
+
+        self.permissions = {}
+
+    def allow(self, node_id, action):
+
+        self.permissions.setdefault(node_id, []).append(action)
+
+    def check(self, node_id, action):
+
+        return action in self.permissions.get(node_id, [])
