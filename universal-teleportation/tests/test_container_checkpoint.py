dd9842b66324ffb49fceddadd9a45bf5ee6125d0
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:19:06 +0300

    Add test for checkpoint_container function

--- universal-teleportation/tests/test_container_checkpoint.py
+++ universal-teleportation/tests/test_container_checkpoint.py
@@ -1,1 +1,9 @@
+from runtime_adapters.container_checkpoint import checkpoint_container
 
+def test_checkpoint():
+
+    container_id = "example"
+
+    checkpoint = checkpoint_container(container_id)
+
+    assert checkpoint == "uat-checkpoint"
