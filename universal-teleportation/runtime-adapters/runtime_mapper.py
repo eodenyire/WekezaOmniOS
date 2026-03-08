cf47c19f4c0c0a584e0a4bb7dc60b137d6338b40
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 18:40:30 +0300

    Add RuntimeMapper class for runtime adapter selection

--- universal-teleportation/runtime-adapters/runtime_mapper.py
+++ universal-teleportation/runtime-adapters/runtime_mapper.py
@@ -1,1 +1,16 @@
+class RuntimeMapper:
+    """
+    Selects the appropriate runtime adapter.
+    """
 
+    def __init__(self):
+
+        self.adapters = {}
+
+    def register_adapter(self, os_name, adapter):
+
+        self.adapters[os_name] = adapter
+
+    def get_adapter(self, os_name):
+
+        return self.adapters.get(os_name)
