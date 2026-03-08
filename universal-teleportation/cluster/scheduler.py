fd988db25ddefedf85dc2d37cf7ba3dca8231cc5
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 14:45:20 +0300

    Create Scheduler class for workload targeting
    Added a Scheduler class to manage workload distribution.

--- universal-teleportation/cluster/scheduler.py
+++ universal-teleportation/cluster/scheduler.py
@@ -1,1 +1,8 @@
+class Scheduler:
+    """
+    Decides where a teleported workload should go.
+    """
 
+    def choose_target(self, nodes):
+        for node_id, node in nodes.items():
+            return node
