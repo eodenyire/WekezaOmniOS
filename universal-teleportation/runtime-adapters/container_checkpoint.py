4bf1c4dc2d96ad8e20b3101eb468903093daf727
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:17:03 +0300

    Update container_checkpoint.py

--- universal-teleportation/runtime-adapters/container_checkpoint.py
+++ universal-teleportation/runtime-adapters/container_checkpoint.py
@@ -1,1 +1,16 @@
+import subprocess
 
+def checkpoint_container(container_id, checkpoint_name="uat-checkpoint"):
+    """
+    Create container checkpoint.
+    """
+
+    subprocess.run([
+        "docker",
+        "checkpoint",
+        "create",
+        container_id,
+        checkpoint_name
+    ])
+
+    return checkpoint_name
