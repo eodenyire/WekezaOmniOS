166a2f423ae8c46b8effc62d5c922b0f3563f71a
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:04:50 +0300

    Implement AuthManager for node authentication
    Added AuthManager class to handle node authentication.

--- universal-teleportation/security/auth_manager.py
+++ universal-teleportation/security/auth_manager.py
@@ -1,1 +1,17 @@
+class AuthManager:
+    """
+    Validates node authentication.
+    """
 
+    def __init__(self):
+        self.trusted_nodes = {}
+
+    def register_node(self, node_id, certificate):
+
+        self.trusted_nodes[node_id] = certificate
+
+    def authenticate(self, node_id, certificate):
+
+        trusted = self.trusted_nodes.get(node_id)
+
+        return trusted == certificate
