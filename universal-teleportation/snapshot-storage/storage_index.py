f47045983ee843266aecf99ea8e6e71cb698ec50
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:29:11 +0300

    Add StorageIndex class for managing snapshots

--- universal-teleportation/snapshot-storage/storage_index.py
+++ universal-teleportation/snapshot-storage/storage_index.py
@@ -1,1 +1,16 @@
+class StorageIndex:
 
+    def __init__(self):
+        self.index = {}
+
+    def register_snapshot(self, snapshot_id, location):
+
+        self.index[snapshot_id] = location
+
+    def get_snapshot(self, snapshot_id):
+
+        return self.index.get(snapshot_id)
+
+    def list_snapshots(self):
+
+        return list(self.index.keys())
