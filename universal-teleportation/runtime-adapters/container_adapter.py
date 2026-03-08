a53c8fac3efc14d55f9f0d64c3f16a36d7a4490e
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 14:37:15 +0300

    Implement container state capture function
    Added a function to capture the state of a Docker container using checkpoints.

--- universal-teleportation/runtime-adapters/container_adapter.py
+++ universal-teleportation/runtime-adapters/container_adapter.py
@@ -1,1 +1,13 @@
+def capture_container(container_id):
+    """
+    Capture container state using Docker checkpoint
+    """
+    import subprocess
 
+    subprocess.run([
+        "docker",
+        "checkpoint",
+        "create",
+        container_id,
+        "uat-checkpoint"
+    ])
