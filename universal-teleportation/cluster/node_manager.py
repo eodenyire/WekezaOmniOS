6c14f76707c97a88457933ab91cd3cefb826492d
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 14:44:23 +0300

    Implement NodeManager to manage node registration

--- universal-teleportation/cluster/node_manager.py
+++ universal-teleportation/cluster/node_manager.py
@@ -1,1 +1,15 @@
+from node_registry import NodeRegistry
 
+class NodeManager:
+
+    def __init__(self):
+        self.registry = NodeRegistry()
+
+    def add_node(self, node_id, address):
+        self.registry.register_node(node_id, address)
+
+    def get_node(self, node_id):
+        return self.registry.get_node(node_id)
+
+    def list_nodes(self):
+        return self.registry.list_nodes()
