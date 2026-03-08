bc152210878a129dcabb97df2a45658f08abea2d
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:49:31 +0300

    Add MigrationState class for managing migration states

--- universal-teleportation/live-migration/migration_state.py
+++ universal-teleportation/live-migration/migration_state.py
@@ -1,1 +1,13 @@
+class MigrationState:
 
+    def __init__(self):
+
+        self.state = "INITIAL"
+
+    def update(self, new_state):
+
+        self.state = new_state
+
+    def get_state(self):
+
+        return self.state
