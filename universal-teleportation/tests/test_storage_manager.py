b96a75eb1614ce1e5777c35460a753c2b8264edc
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 15:34:05 +0300

    Implement test for StorageManager snapshot listing
    Add a test for listing snapshots in StorageManager

--- universal-teleportation/tests/test_storage_manager.py
+++ universal-teleportation/tests/test_storage_manager.py
@@ -1,1 +1,11 @@
+from snapshot_storage.storage_manager import StorageManager
+from snapshot_storage.local_storage import LocalStorage
 
+def test_storage():
+
+    backend = LocalStorage()
+    manager = StorageManager(backend)
+
+    snapshots = manager.list_snapshots()
+
+    assert isinstance(snapshots, list)
