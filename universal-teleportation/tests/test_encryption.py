d4735630e251230ddef9cdc87b8f87ded85d1935
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:32:31 +0300

    Add test for encryption and decryption functionality

--- universal-teleportation/tests/test_encryption.py
+++ universal-teleportation/tests/test_encryption.py
@@ -1,1 +1,13 @@
+from security.encryption import EncryptionManager
 
+def test_encrypt_decrypt():
+
+    enc = EncryptionManager()
+
+    data = b"hello"
+
+    encrypted = enc.encrypt(data)
+
+    decrypted = enc.decrypt(encrypted)
+
+    assert decrypted == data
