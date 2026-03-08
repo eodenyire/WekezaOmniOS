a923569d24d912ec46bbbcceb0df909312ac791f
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:34:07 +0300

    Add authentication test for AuthManager

--- universal-teleportation/tests/test_security_auth.py
+++ universal-teleportation/tests/test_security_auth.py
@@ -1,1 +1,9 @@
+from security.auth_manager import AuthManager
 
+def test_authentication():
+
+    auth = AuthManager()
+
+    auth.register_node("node1", "cert123")
+
+    assert auth.authenticate("node1", "cert123")
