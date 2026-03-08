5476a2f86ead391516f3b6ce6688c6d7393b243e
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 14:36:47 +0300

    Update network_transfer.py

--- universal-teleportation/transfer-layer/network_transfer.py
+++ universal-teleportation/transfer-layer/network_transfer.py
@@ -1,1 +1,8 @@
+import shutil
+import socket
 
+def send_snapshot(snapshot_path, target_host, target_path):
+    """
+    Transfer snapshot to remote node
+    """
+    shutil.copy(snapshot_path, target_path)
