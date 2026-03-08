bdd6e84a836c5a17b7cde44abbb58eef5c85e655
Emmanuel Odenyire <eodenyire@gmail.com>
2026-03-08 16:05:58 +0300

    Update node_identity.py

--- universal-teleportation/security/node_identity.py
+++ universal-teleportation/security/node_identity.py
@@ -1,1 +1,15 @@
+import uuid
 
+class NodeIdentity:
+
+    def __init__(self, node_name):
+
+        self.node_name = node_name
+        self.node_id = str(uuid.uuid4())
+
+    def get_identity(self):
+
+        return {
+            "node_name": self.node_name,
+            "node_id": self.node_id
+        }
